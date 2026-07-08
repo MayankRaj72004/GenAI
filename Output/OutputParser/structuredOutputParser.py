af3c403b6b1042ccfe4454871d302eaf1658504b
mayank raj <dishuyadav8791@gmail.com>
2026-07-09 01:59:22 +0530

    Learning and Implementation of  Output Parsers

---
+++ Output/OutputParser/structuredOutputParser.py
@@ -0,0 +1,40 @@
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv() 
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name='fact_1',description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2',description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3',description='Fact 3 about the topic'),
+    ResponseSchema(name='fact_4',description='Fact 4 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+
+template = PromptTemplate(
+    template='Give three fact about the {topic} \n {format_instruction}',
+    input_variables = ['topic'],
+    partial_variables = {'format_instruction':parser.get_format_instructions()}
+)
+
+prompt = template.invoke({'topic':'black hole'})
+
+result = model.invoke(prompt)
+
+final_result = parser.parse(result.content)
+
+
+print(final_result)
